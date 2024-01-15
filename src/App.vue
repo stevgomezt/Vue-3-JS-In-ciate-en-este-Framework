3fda4a90b096d7ca36b07399f858a32ab0121a07
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2024-01-15 11:59:26 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Componente app.vue</h1>
+        <Car />
+    </div>
+</template>
+
+<script>
+import Car from "./components/Car.vue";
+export default {
+    components: {
+        Car,
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Car.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <h2>Componente car.vue</h2>
+        <p>Marca: {{ brand }}</p>
+        <p>
+            Modelo
+            <!-- asigna key unica cuando existen valores repetidos -->
+            <span v-for="(modelo, index) in modelos" :key="modelo">
+                <!-- asigna key unica cuando los valores son distintos -->
+                <!-- <span v-for="modelo in modelos" :key="modelo"> -->
+                {{ modelo }}
+            </span>
+        </p>
+        <p>Colores: {{ color }}</p>
+        <p>Color Seleccionado: {{ color[0] }}</p>
+        <p>Titulo: {{ mensaje.title }}</p>
+        <p>Titulo: {{ mensaje.text1 }}</p>
+        <p>Titulo: {{ mensaje.text2 }}</p>
+        <p>Titulo: {{ mensaje.text3 }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    // V2
+    // data() {
+    //     return {
+    //         brand: "audi",
+    //         modelo: "A4",
+    //         color: ["azul", "blanco", "rojo"], //Variable tipo array
+    //         mensaje: {
+    //             title: "Titulo",
+    //             text1: "Mensaje1",
+    //             text2: "Mensaje2",
+    //             text3: "Mensaje3",
+    //         }, //Variable de tipo objeto
+    //     };
+    // },
+
+    //V3
+    setup() {
+        const brand = "audi";
+        const modelos = ["A4", "R8", "Q3"];
+        const color = ["azul", "blanco", "rojo"]; //Variable tipo array
+        const mensaje = {
+            title: "Titulo",
+            text1: "Mensaje1",
+            text2: "Mensaje2",
+            text3: "Mensaje3",
+        }; //Variable de tipo objeto
+
+        return {
+            brand,
+            modelos,
+            color,
+            mensaje,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
